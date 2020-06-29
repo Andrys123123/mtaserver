7e2284c0e3b03141e924a0b2243f87f0067cbfc7
Andrys123123 <kbandrasl@gmail.com>
2020-06-29 22:02:25 +0200

    Authentication

--- mods/deathmatch/resources/players/authentication.lua
+++ mods/deathmatch/resources/players/authentication.lua
@@ -7,10 +7,10 @@ end
 
 
 -- create account
-addCommandHandler('register', function (player, command, username, password) 
+addCommandHandler('accregister', function (player, command, username, password) 
     if not username or not password then
-            return outputChatBox('SYNTAX: /' .. command .. ' [username] [password]', player, 255, 255, 255)
-        end
+        return outputChatBox('SYNTAX: /' .. command .. ' [username] [password]', player, 255, 255, 255)
+    end
     
     --cehck if an account with that username is already exist
     if getAccount(username) then
@@ -21,26 +21,26 @@ addCommandHandler('register', function (player, command, username, password)
 
     -- is this password valid
     if not isPasswordValid('password') then 
-        return outputChatBox('The passoword not valid', player, 255, 100,100)
+        return outputChatBox('The passoword not valid', player, 255, 100, 100)
     end 
 
 
     -- create a hash of the password
-    passwordHash(password, 'bcrypt', {}, function(hashedPassword) 
+    passwordHash(password, 'bcrypt', {}, function (hashedPassword) 
         -- create account
         local account = addAccount(username, hashedPassword)
-        setAccountData(account, 'hashedPassword', hashedPassword)
+        setAccountData(account, 'hashed_password', hashedPassword)
         
         -- let the user know of  our succes
         outputChatBox('Your account has been succesfully created! You may now login with /acclogin', player, 100, 255, 100)
     
     end)
-end)
+end, false, false)
 
 
 
---login
-addCommandHandler('acclogin', function(player, command, username, password)
+--login to their account
+addCommandHandler('acclogin', function (player, command, username, password)
     if not username or not password then
         return outputChatBox('SYNTAX: /' .. command .. ' [username] [password]', player, 255, 255, 255)
     end
@@ -48,28 +48,30 @@ addCommandHandler('acclogin', function(player, command, username, password)
     local account = getAccount(username)
 
     if not account then
-        outputChatBox('No such account could be found with that username or password.', player, 255, 100, 100)
+        return outputChatBox('No such account could be found with that username or password.', player, 255, 100, 100)
     end 
 
 
     local hashedPassword = getAccountData(account, 'hashed_password')
     passwordVerify(password, hashedPassword, function(isValid) 
         if not isValid then 
-            outputChatBox('No such account could be found with that username or password.', player, 255, 100, 100)
+            return outputChatBox('No such account could be found with that username or password.', player, 255, 100, 100)
         end
 
         if logIn(player, account, hashedPassword) then 
             return outputChatBox('Yuo have succesfully logged in!', player, 100, 255, 100)
         end
+        
         return outputChatBox('An unknown error occuered while attempting to authenticate', player, 255, 100, 100)
 
     end)
 
 end, false, false)
 
-
-
 --logout
 
-
+addCommandHandler('acclogout', function (player, command) 
+    logOut(player)
+    outputChatBox('Succesfully logged out.', player, 100, 255, 100)
+end)
 
